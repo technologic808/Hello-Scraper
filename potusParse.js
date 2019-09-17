0c002deb8233426378f0b713060d7ede925fd3d5
technologic808 <20627603+technologic808@users.noreply.github.com>
2019-09-17 22:21:01 +0530

    Update potusParse.js
    wrap into a function and export as a module

--- potusParse.js
+++ potusParse.js
@@ -1,12 +1,15 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
-const url = 'https://en.wikipedia.org/wiki/George_Washington';
 
-rp(url)
-    .then(function(html){
-        console.log($('.firstHeading', html).text());
-        console.log($('.bday', html).text());
-    })
-    .catch(function(err){
-        // handle error
-    });
+const potusParse = function(url){
+    return rp(url)
+        .then(function(html){
+            console.log($('.firstHeading', html).text());
+            console.log($('.bday', html).text());
+        })
+        .catch(function(err){
+            // handle error
+        });
+};
+
+module.exports = potusParse;
