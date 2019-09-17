00b73b4335870241da3e2889c40304e7de6d3cd2
technologic808 <20627603+technologic808@users.noreply.github.com>
2019-09-17 21:59:35 +0530

    Get specific html tags using CheerioJS
    Names of the presidents are in <big> tags with hyperlinks inside them.
    
    so big(tag) >(having) a(hyperlink)

--- potusScraper.js
+++ potusScraper.js
@@ -1,10 +1,12 @@
 const rp = require('request-promise');
+const $ = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
 rp(url)
     .then(function (html) {
         // success
-        console.log(html);   
+        console.log($('big > a', html).length);   
+        console.log($('big > a', html));
     })
     .catch(function(err){
         //handle error
